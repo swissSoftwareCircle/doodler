390331adc66a53d44d71fd3fc72d878c9c005699
sscbalti <baltisar.oswald@gmail.com>
2012-01-29 00:19:04 +0100

    added logging to application. at the momment jdk14 logging with slf4j

--- doodler-core/src/main/java/ch/ssc/doodler/service/DoodleRESTClient.java
+++ doodler-core/src/main/java/ch/ssc/doodler/service/DoodleRESTClient.java
@@ -6,6 +6,9 @@ package ch.ssc.doodler.service;
 import javax.ejb.Stateless;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ch.ssc.doodler.generated.PollType;
 
 import com.sun.jersey.api.client.Client;
@@ -17,12 +20,17 @@ import com.sun.jersey.api.client.WebResource;
  */
 @Stateless
 public class DoodleRESTClient {
+	
 	public static final String DOODLE_URL = "http://doodle-test.com/api1WithoutAccessControl/polls/";
+	
+	private static final Logger logger = LoggerFactory.getLogger(DoodleRESTClient.class);
 	
 	Client client = Client.create();
 	 
 	
 	public PollType getPoll(String doodleId) {
+		
+		logger.debug("*************************** test");
 		
 		WebResource wr = client.resource(DOODLE_URL + doodleId);
 		PollType pt = wr.accept(MediaType.APPLICATION_XML_TYPE).get(PollType.class);
